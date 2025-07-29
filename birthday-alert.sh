debabfe62b8491eb3d61d8a8febcfbb74abb3382
stonecoder-actual <samstnstrt@gmail.com>
2025-07-29 15:26:33 -0400

    venv check

--- birthday-alert.sh
+++ birthday-alert.sh
@@ -3,6 +3,12 @@
 # Navigate to the project directory
 cd /home/sam/birthday-alert
 
+
+# Check if the virtual environment exists, if not create it
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
 # Activate the virtual environment
 source venv/bin/activate
 
